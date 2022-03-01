93a92d72675de71a936b981baa0fb4213279e912
gksonar <gauravsonar8180@gmail.com>
2022-03-01 11:45:14 +0530

    PWA

--- sw.js
+++ sw.js
@@ -1,51 +1,14 @@
-self.addEventListener("install", function(event) {
-  event.waitUntil(preLoad());
-});
-
-var preLoad = function(){
-  console.log("Installing web app");
-  return caches.open("offline").then(function(cache) {
-    console.log("caching index and important routes");
-    return cache.addAll(["/blog/", "/blog", "/", "/contact", "/resume", "/offline.html"]);
+self.addEventListener('install', async function () {
+    const cache = await caches.open('static01');
+    cache.add('./offline.html')
   });
-};
-
-self.addEventListener("fetch", function(event) {
-  event.respondWith(checkResponse(event.request).catch(function() {
-    return returnFromCache(event.request);
-  }));
-  event.waitUntil(addToCache(event.request));
-});
-
-var checkResponse = function(request){
-  return new Promise(function(fulfill, reject) {
-    fetch(request).then(function(response){
-      if(response.status !== 404) {
-        fulfill(response);
-      } else {
-        reject();
-      }
-    }, reject);
+  
+  self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
   });
-};
-
-var addToCache = function(request){
-  return caches.open("offline").then(function (cache) {
-    return fetch(request).then(function (response) {
-      console.log(response.url + " was cached");
-      return cache.put(request, response);
-    });
+  
+  self.addEventListener('fetch', event => {
+    const request = event.request;
+    event.respondWith(networkFirst(request));
   });
-};
-
-var returnFromCache = function(request){
-  return caches.open("offline").then(function (cache) {
-    return cache.match(request).then(function (matching) {
-     if(!matching || matching.status == 404) {
-       return cache.match("offline.html");
-     } else {
-       return matching;
-     }
-    });
-  });
-};
+  
